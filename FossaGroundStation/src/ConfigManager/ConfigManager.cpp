af64cf01552d95d50257f57da44e82ee14b610d0
Sharkusa <38981078+Sharkusa@users.noreply.github.com>
2020-01-11 11:05:57 -0600

    Update ConfigManager.cpp

--- FossaGroundStation/src/ConfigManager/ConfigManager.cpp
+++ FossaGroundStation/src/ConfigManager/ConfigManager.cpp
@@ -34,6 +34,7 @@ ConfigManager::ConfigManager()
   {      0x3c,       21,        22,       16,           0,        25,     false,    18,      0,     33,     32,    14,      19,     27,     5,     0.0f, "TTGO LoRa 32 V2 Modified with module SX126x (crystal)"  },// @TCRobotics
   {      0x3c,       21,        22,       16,           0,        25,     false,     5,      0,      2,     13,    26,      19,     23,    18,     1.6f, "Custom ESP32 Wroom + SX126x DRF1268T (TCX0) (5, 2, 26, 13)"  }, // @sdey76
   {      0x3c,       21,        22,       16,           0,        25,     false,     5,      0,     26,     12,    14,      19,     23,    18,     1.6f, "Custom ESP32 Wroom + SX126x DRF1268T (TCX0) (5, 26, 14, 12)"  }, // @imants
+  {      0x3c,        4,        15,       16,           0,         2,     false,     5,      0,     13,     27,    14,      19,     23,    18,     0.0f, "Custom TTGO WiFi + SX126x E22-400M30S"}, // @Longhorn
   })
 {
   server.on(ROOT_URL, [this]{ handleRoot(); });
